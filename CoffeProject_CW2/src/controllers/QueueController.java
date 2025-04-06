1583477ac13311c737da0492f1f7d6b384826689
Voidhi <l.lakomicki@gmail.com>
2025-04-06 16:44:56 +0100

    continuing the mVC pattern + view

--- CoffeProject_CW2/src/controllers/QueueController.java
+++ CoffeProject_CW2/src/controllers/QueueController.java
@@ -1,5 +1,27 @@
 package controllers;
 
-public class QueueController {
+import models.MyData;
+import views.QueueInterface;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class QueueController {
+	private MyData myData; //model
+	private QueueInterface view;
+	
+	public QueueController(MyData myData, QueueInterface view) {
+		this.myData = myData;
+		this.view = view;
+		view.addSetListener( new SetListener() );
+	}
+	
+
+	public class SetListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			// TODO :
+			// action performed on the view (eg : changing simulation speed)
+			// -> must send info to the model,  myData.setXXX()
+		}
+	}
 }
--- CoffeProject_CW2/src/views/QueueInterface.java
+++ CoffeProject_CW2/src/views/QueueInterface.java
@@ -1,8 +1,55 @@
 package views;
 
-public class QueueInterface implements Observers {
+import java.awt.event.ActionListener;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+
+public class QueueInterface extends Application implements Observers {
+	// GUI element :
+	private BorderPane Screen; // top : title, Center : VBox layout, Bottom : slider
+	private VBox layout; // up : current queue | down : servers list (Tilepane)
+	private TilePane myServers; // scrollable
+	// Data :
 	
-	public void Update() {
-		
+	
+	// To send info to the controller :
+	public void addSetListener(ActionListener al) {
+		// TODO : link buttons to event :
+		// eg : mySlider.addActionListener(al);
 	}
+	// To receive info from the model MyData directly :
+	public void Update() {
+		// to update the info on the view
+		// eg. the queue, servers' stats...
+	}
+	
+	
+	
+
+	/**
+	 * Init the scene
+	 */
+	@Override
+	public void start(Stage primaryStage){
+		// Init the compo of the scene :
+		Screen = new BorderPane();
+		layout = new VBox();
+		
+		// Show the scene :
+		Scene scene = new Scene(layout, 400, 600);
+		primaryStage.setTitle("Coffee Shop");
+        primaryStage.setScene(scene);
+        primaryStage.setOnCloseRequest(e -> {
+        	e.consume();
+        	//TODO : send report here
+        });
+        primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+        launch(args);
+    }
 }

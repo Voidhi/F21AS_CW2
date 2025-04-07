2254da55397e02935bc17569707ca1bed984d811
Matthias1001 <m.franchini@hotmail.fr>
2025-04-07 15:24:34 +0100

    Update QueueInterface.java

--- CoffeProject_CW2/src/views/QueueInterface.java
+++ CoffeProject_CW2/src/views/QueueInterface.java
@@ -137,7 +137,6 @@ public class QueueInterface extends Application implements Observers {
 
 	public void startSimulation(int numServers, int numCustomers) {
 		HashMap<String,Customer> customerHashMap = DocumentManager.CreateCustomers();
-		HashMap<Customer,Integer> customerOrders = DocumentManager.getCustomerOrders(); //returns how many items each customer have ordered
 		for (Map.Entry<String, Customer> entry : customerHashMap.entrySet()) {
 			Customer customer = entry.getValue();
 			customerList.add(customer);
